7cbe08b1a995268c2315f9c73f6f65d18a53662f
Mitch Fultz <mitchfultz@gmail.com>
2024-02-08 10:58:32 -0700

    Updated models to include latest gpt 3.5 turbo

--- app/src/core/chat/openai.ts
+++ app/src/core/chat/openai.ts
@@ -187,12 +187,12 @@ export const maxTokensByModel = {
   'gpt-4-0613': 8192,
   'gpt-4-32k': 32768,
   'gpt-4-32k-0613': 32768,
-  'gpt-3.5-turbo-1106': 16384,
+  'gpt-3.5-turbo-0125': 16385,
+  'gpt-3.5-turbo-1106': 16385,
   'gpt-3.5-turbo': 4096,
-  'gpt-3.5-turbo-16k': 16384,
+  'gpt-3.5-turbo-16k': 16385,
   'gpt-3.5-turbo-instruct': 4096,
   'gpt-3.5-turbo-0613': 4096,
-  'gpt-3.5-turbo-16k-0613': 16384,
-  'gpt-3.5-turbo-0301': 4096
+  'gpt-3.5-turbo-16k-0613': 16385
 }
 
--- app/src/global-options/parameters.tsx
+++ app/src/global-options/parameters.tsx
@@ -1,116 +1,130 @@
-import { defaultModel } from '../core/chat/openai'
-import { type OptionGroup } from '../core/options/option-group'
+import { defaultModel } from "../core/chat/openai";
+import { type OptionGroup } from "../core/options/option-group";
 
 export const parameterOptions: OptionGroup = {
-  id: 'parameters',
+  id: "parameters",
   options: [
     {
-      id: 'model',
+      id: "model",
       defaultValue: defaultModel,
       resettable: false,
-      scope: 'user',
-      displayOnSettingsScreen: 'chat',
+      scope: "user",
+      displayOnSettingsScreen: "chat",
       displayAsSeparateSection: true,
       displayInQuickSettings: {
-        name: 'Model',
+        name: "Model",
         displayByDefault: true,
-        label: (value) => value
+        label: (value) => value,
       },
       renderProps: (value, options, context) => ({
-        type: 'select',
-        label: 'Model',
-        description: value?.includes('32') && context.intl.formatMessage(
-          {
-            defaultMessage: 'Note: This model will only work if your OpenAI account has been granted you have been given access to it. <a>Request access here.</a>'
-          },
-          {
-            a: (text: string) => <a href="https://openai.com/waitlist/gpt-4-api" target="_blank" rel="noreferer noreferrer">{text}</a>
-          } as any
-        ),
+        type: "select",
+        label: "Model",
+        description:
+          value?.includes("32") &&
+          context.intl.formatMessage(
+            {
+              defaultMessage:
+                "Note: This model will only work if your OpenAI account has been granted you have been given access to it. <a>Request access here.</a>",
+            },
+            {
+              a: (text: string) => (
+                <a
+                  href="https://openai.com/waitlist/gpt-4-api"
+                  target="_blank"
+                  rel="noreferer noreferrer"
+                >
+                  {text}
+                </a>
+              ),
+            } as any
+          ),
         options: [
           {
-            label: 'GPT-4 Turbo Preview (latest)',
-            value: 'gpt-4-turbo-preview'
+            label: "GPT-4 Turbo Preview (latest)",
+            value: "gpt-4-turbo-preview",
           },
           {
-            label: 'GPT-4 Turbo 0125 Preview',
-            value: 'gpt-4-0125-preview'
+            label: "GPT-4 Turbo 0125 Preview",
+            value: "gpt-4-0125-preview",
           },
           {
-            label: 'GPT-4 Turbo 1106 Preview',
-            value: 'gpt-4-1106-preview'
+            label: "GPT-4 Turbo 1106 Preview",
+            value: "gpt-4-1106-preview",
           },
           {
-            label: 'GPT-4 Vision Preview',
-            value: 'gpt-4-vision-preview'
+            label: "GPT-4 Vision Preview",
+            value: "gpt-4-vision-preview",
           },
           {
-            label: 'GPT-4 (latest)',
-            value: 'gpt-4'
+            label: "GPT-4 Classic",
+            value: "gpt-4",
           },
           {
-            label: 'GPT-4 0613',
-            value: 'gpt-4-0613'
+            label: "GPT-4 Classic 0613",
+            value: "gpt-4-0613",
           },
           {
-            label: 'GPT-4 32k (latest)',
-            value: 'gpt-4-32k'
+            label: "GPT-4 32k",
+            value: "gpt-4-32k",
           },
           {
-            label: 'GPT-4 32k 0613',
-            value: 'gpt-4-32k-0613'
+            label: "GPT-4 32k 0613",
+            value: "gpt-4-32k-0613",
           },
           {
-            label: 'GPT-3.5 Turbo (latest)',
-            value: 'gpt-3.5-turbo'
+            label: "GPT-3.5 Turbo 0125",
+            value: "gpt-3.5-turbo-0125",
           },
           {
-            label: 'GPT-3.5 Turbo 1106',
-            value: 'gpt-3.5-turbo-1106'
+            label: "GPT-3.5 Turbo 1106",
+            value: "gpt-3.5-turbo-1106",
           },
           {
-            label: 'GPT-3.5 Turbo 16k (latest)',
-            value: 'gpt-3.5-turbo-16k'
+            label: "GPT-3.5 Turbo",
+            value: "gpt-3.5-turbo",
           },
           {
-            label: 'GPT-3.5 Turbo Instruct',
-            value: 'gpt-3.5-turbo-instruct'
+            label: "GPT-3.5 Turbo 16k",
+            value: "gpt-3.5-turbo-16k",
           },
           {
-            label: 'GPT-3.5 Turbo 0613',
-            value: 'gpt-3.5-turbo-0613'
+            label: "GPT-3.5 Turbo Instruct",
+            value: "gpt-3.5-turbo-instruct",
           },
           {
-            label: 'GPT-3.5 Turbo 16k 0613',
-            value: 'gpt-3.5-turbo-16k-0613'
+            label: "GPT-3.5 Turbo 0613",
+            value: "gpt-3.5-turbo-0613",
           },
           {
-            label: 'GPT-3.5 Turbo 0301',
-            value: 'gpt-3.5-turbo-0301'
-          }
-        ]        
-      })
+            label: "GPT-3.5 Turbo 16k 0613",
+            value: "gpt-3.5-turbo-16k-0613",
+          },
+        ],
+      }),
     },
     {
-      id: 'temperature',
+      id: "temperature",
       defaultValue: 0.5,
       resettable: true,
-      scope: 'chat',
-      displayOnSettingsScreen: 'chat',
+      scope: "chat",
+      displayOnSettingsScreen: "chat",
       displayAsSeparateSection: true,
       displayInQuickSettings: {
-        name: 'Temperature',
+        name: "Temperature",
         displayByDefault: true,
-        label: (value) => 'Temperature: ' + value.toFixed(1)
+        label: (value) => "Temperature: " + value.toFixed(1),
       },
       renderProps: (value, options, context) => ({
-        type: 'slider',
-        label: 'Temperature: ' + value.toFixed(1),
+        type: "slider",
+        label: "Temperature: " + value.toFixed(1),
         min: 0,
         max: 1,
         step: 0.1,
-        description: context.intl.formatMessage({ defaultMessage: "The temperature parameter controls the randomness of the AI's responses. Lower values will make the AI more predictable, while higher values will make it more creative." })
-      })
-    }
-  ]
-}
+        description: context.intl.formatMessage({
+          defaultMessage:
+            "The temperature parameter controls the randomness of the AI's responses. Lower values will make the AI more predictable, while higher values will make it more creative.",
+        }),
+      }),
+    },
+  ],
+};
